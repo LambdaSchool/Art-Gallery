7c89b780b6f9f4ed3e07f17374da6cdda0ea9bb8
Rwoltx <richardx1016@gmail.com>
2018-08-29 19:05:22 -0400

    Files accidentally not included in first push

--- ArtGallery/Painting.swift
+++ ArtGallery/Painting.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+class Painting {
+    let image: UIImage
+    var isLiked: Bool
+    
+    
+    init(of image: UIImage, isLiked: Bool = true){
+        self.image = image
+        self.isLiked = isLiked
+    }
+    
+    
+}
--- ArtGallery/PaintingController.swift
+++ ArtGallery/PaintingController.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+class PaintingController {
+    
+    ///CRUD for databases and model controllers this is usually the four functions -- Create-Read-Update-Delete
+    
+    
+    init(){
+        self.loadPaintingsFromAssets()
+    }
+    
+    var paintings: [Painting] = []
+    
+    func loadPaintingsFromAssets(){
+        for index in 1...12{
+            let indexString = "Image\(index)"
+            guard let paintingFromAsset = UIImage(named: indexString) else {return}
+            let newPaintingObject = Painting(of: paintingFromAsset)
+            paintings.append(newPaintingObject)
+        }
+    }
+}
