2808ec3d8b357fa91bc6392539862697871acc4c
Dillon McElhinney <dillon.mce@gmail.com>
2018-08-29 13:08:19 -0600

    Add Painting model

---
+++ ArtGallery/Painting.swift
@@ -0,0 +1,18 @@
+//
+//  Painting.swift
+//  ArtGallery
+//
+//  Created by Dillon McElhinney on 8/29/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class Painting {
+    let image: UIImage
+    var isLiked = false
+    
+    init (image: UIImage) {
+        self.image = image
+    }
+}
