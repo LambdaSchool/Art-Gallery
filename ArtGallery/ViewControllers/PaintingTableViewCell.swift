8fdf315968f780ff6e1b0745655d7a03837de3da
Rwoltx <richardx1016@gmail.com>
2018-09-05 22:51:15 -0400

    Added the needed delegate

--- ArtGallery/ViewControllers/PaintingTableViewCell.swift
+++ ArtGallery/ViewControllers/PaintingTableViewCell.swift
@@ -8,30 +8,53 @@
 
 import UIKit
 
-class PaintingTableViewCell: UITableViewCell {
-    
-    var painting: Painting?
 
+protocol PaintingTableViewCellDelegate: class {
+    func likeButtonWasTapped(on cell: PaintingTableViewCell)
+}
+
+class PaintingTableViewCell: UITableViewCell {
+
+    
+    
+    
+    
+    
+    var painting: Painting? {
+        didSet {
+            updateViews()
+        }
+    }
+    var delegate: PaintingTableViewCellDelegate?
+    
     @IBOutlet weak var paintingImageView: UIImageView!
     
     @IBOutlet weak var button: UIButton!
     
     @IBAction func likeButton(_ sender: Any) {
+        delegate?.likeButtonWasTapped(on: self)
     }
     
     func updateViews(){
-    
+        
     guard let painting = painting else {return}
     paintingImageView.image = painting.image
-        
+    
         if painting.isLiked{
             button.setTitle("Unlike", for: .normal)
         } else if !painting.isLiked{
             button.setTitle("Like", for: .normal)
         }
+        }
+    func toggle(for painting: Painting)  {
         
-    
-    
+        if painting.isLiked == false {
+            painting.isLiked = true
+        } else if painting.isLiked == true {
+            painting.isLiked = false
+        }
     }
+    
+    
     
 }
