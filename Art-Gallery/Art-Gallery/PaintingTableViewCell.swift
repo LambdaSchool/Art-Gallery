4316aeb89742e510f72b30627f1ef617a199f562
Paul Yi <yipaulc@gmail.com>
2018-10-03 18:13:06 -0700

    Set up cell

--- Art-Gallery/Art-Gallery/PaintingTableViewCell.swift
+++ Art-Gallery/Art-Gallery/PaintingTableViewCell.swift
@@ -3,6 +3,14 @@ import UIKit
 class PaintingTableViewCell: UITableViewCell {
     
     
+    @IBOutlet weak var portraitView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
     
+    weak var delegate: PaintingTableViewCellDelegate?
     
+    @IBAction func toggledAppreciation(_ sender: Any) {
+        
+        delegate?.likeButtonWasTapped(on: self)
+        
+    }
 }
