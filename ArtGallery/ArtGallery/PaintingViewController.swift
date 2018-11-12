6f7120084f2d75ac323649454221cca1728e33a5
Ben Hakes <bhhakes@gmail.com>
2018-11-12 10:35:19 -0600

    cleanse code + add TV IBoutlet to VC

--- ArtGallery/ArtGallery/PaintingViewController.swift
+++ ArtGallery/ArtGallery/PaintingViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  ArtGallery
-//
-//  Created by Benjamin Hakes on 11/12/18.
-//  Copyright © 2018 Benjamin Hakes. All rights reserved.
-//
 
 import UIKit
 
@@ -12,9 +5,11 @@ class PaintingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    @IBOutlet weak var tableView: UITableView!
+    
 
 }
 
