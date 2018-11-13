87d5a86f7666b276e7cc418f95f663f52880c94d
AustinBCole <austin.cole.chileno@gmail.com>
2018-11-12 23:57:59 -0700

    Add assits folder.

--- Art Gallery/Art Gallery/PaintingModel.swift
+++ Art Gallery/Art Gallery/PaintingModel.swift
@@ -8,22 +8,21 @@ class PaintingModel : NSObject, UITableViewDataSource, PaintingTableViewCellDele
     var imageArray: [UIImage] = []
     var painting = Painting(nil, false)
     
-    func collectPaintingsInArray() -> [Painting] {
-        for counter in 1 ... 12 {
-            let imageName = "image\(counter)"
-            if let image = UIImage(named: imageName) {
-                imageArray.append(image)
-            }
-            
-        }
-        for index in imageArray {
-            var painting = Painting(index, false)
-            paintings += [painting]
-        }
-        
-        return paintings
+    //func collectPaintingsInArray() -> [Painting] {
+    /*for counter in 1 ... 12 {
+    let imageName = "image\(counter)"
+    if let image = UIImage(named: imageName) {
+    imageArray.append(image)
     }
     
+    }
+    for index in imageArray {
+    var painting = Painting(index, false)
+    paintings.append(painting)
+    }
+    print(paintings)
+    return paintings*/
+    //}
     
     
     func tappedLikeButton(on cell: PaintingTableViewCell) {
@@ -38,13 +37,26 @@ class PaintingModel : NSObject, UITableViewDataSource, PaintingTableViewCellDele
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imageArray.count
+        return (imageArray.count - 1)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-        let newPaintings = collectPaintingsInArray()
-        cell.imageView?.image = newPaintings[indexPath.row].image
+        for counter in 1 ... 12 {
+            let imageName = "image\(counter)"
+            if let image = UIImage(named: imageName) {
+                imageArray.append(image)
+            }
+            
+        }
+        for index in imageArray {
+            var painting = Painting(index, false)
+            paintings.append(painting)
+        }
+        print(paintings)
+        
+        
+let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.imageView?.image = paintings[indexPath.row].image
         return cell
         
     }
