59b84ab09a407cf4295ed3b87fb77ef86325c225
Paul Yi <yipaulc@gmail.com>
2019-01-14 11:53:04 -0800

    Set up project

---
+++ ArtGallery/ArtGallery/PaintingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PaintingViewController.swift
+//  ArtGallery
+//
+//  Created by Paul Yi on 1/14/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+import UIKit
+
+class PaintingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
