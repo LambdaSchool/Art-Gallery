7bd0d9c818264347f8a7224d3ce8f9cc5a7a9828
Stuart Sippel <stuartsippel@icloud.com>
2019-01-14 14:38:42 -0600

    Setup interface, create classes, connect outlets, implement viewWillAppear

---
+++ Art Gallery/Art Gallery/Custom Cells/PaintingTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PaintingTableViewCell.swift
+//  Art Gallery
+//
+//  Created by Stuart on 1/14/19.
+//  Copyright © 2019 Stuart. All rights reserved.
+//
+
+import UIKit
+
+class PaintingTableViewCell: UITableViewCell {
+    
+}
---
+++ Art Gallery/Art Gallery/View Controllers/PaintingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PaintingViewController.swift
+//  Art Gallery
+//
+//  Created by Stuart on 1/14/19.
+//  Copyright © 2019 Stuart. All rights reserved.
+//
+
+import UIKit
+
+class PaintingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBOutlet weak var tableView: UITableView!
+}
