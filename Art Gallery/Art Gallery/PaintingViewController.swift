1abddaf2242a43ddc73d47655c56ee5163a1c9f7
Moses Robinson <mosesjrobinson@icloud.com>
2019-01-14 13:04:33 -0800

    setting up storyboard

---
+++ Art Gallery/Art Gallery/PaintingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Art Gallery
+//
+//  Created by Moses Robinson on 1/14/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+import UIKit
+
+class PaintingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
