714f14f47def1fd19362ecb304fca4a582a55cec
Vijay Das <vijaytdas@gmail.com>
2019-01-14 14:24:05 -0600

    add Painting model, main storyboard, custom cell with image view and button

---
+++ Art-Gallery/Art-Gallery/Painting.swift
@@ -0,0 +1,15 @@
+//
+//  Painting.swift
+//  Art-Gallery
+//
+//  Created by Vijay Das on 1/14/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Painting {
+    var image: UIImage
+    var isLiked: Bool = false
+}
