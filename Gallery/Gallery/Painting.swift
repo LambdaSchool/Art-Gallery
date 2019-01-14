f1102437edd2634273f03102157774e460be1772
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-14 14:51:23 -0500

    Add initializer to struct

--- Gallery/Gallery/Painting.swift
+++ Gallery/Gallery/Painting.swift
@@ -11,7 +11,10 @@ import UIKit
 
 struct Painting {
     let image: UIImage
-    var isLiked: Bool = false
+    var isLiked: Bool
     
-   
+    init(image: UIImage, isLiked: Bool = false){
+        self.image = image
+        self.isLiked = isLiked
+    }
 }
