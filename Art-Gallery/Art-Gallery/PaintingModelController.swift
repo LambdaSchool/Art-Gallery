5158ad7b7cb658bf9837189a83a0df3c6d3379ae
Vijay Das <vijaytdas@gmail.com>
2019-01-21 17:14:33 -0600

    add initializer to load images for paintings array

---
+++ Art-Gallery/Art-Gallery/PaintingModelController.swift
@@ -0,0 +1,42 @@
+//
+//  PaintingModelController.swift
+//  Art-Gallery
+//
+//  Created by Vijay Das on 1/21/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// will implement delelgate protocol here but where does it belong?
+
+protocol PaintingTableViewCellDelegate: class {
+    
+}
+
+
+class PaintingModelController {
+    
+    private(set) var paintings: [Painting] = []
+    
+    // need images
+    
+    init() {
+        for index in 1...12 {
+            let name: String = "Image\(index)"
+            guard let image = UIImage(named: name) else { return }
+            let painting = Painting(image: image, isLiked: false)
+            paintings.append(painting)
+        }
+    }
+    
+    // need methods for number of paintings, and painting at
+    
+    
+    
+    
+    
+
+    
+}
--- Art-Gallery/Art-Gallery/PaintingViewController.swift
+++ Art-Gallery/Art-Gallery/PaintingViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class PaintingViewController: UIViewController {
+class PaintingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
     
 
     /*
