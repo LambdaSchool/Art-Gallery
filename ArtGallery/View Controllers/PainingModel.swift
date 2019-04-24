0115dfa941ff630802e451d41a0c63d63d01cc62
Hayden Hastings <haydenhastings32@gmail.com>
2019-04-24 15:57:01 -0600

    Add extensions.

--- ArtGallery/View Controllers/PainingModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PainingModel.swift
-//  ArtGallery
-//
-//  Created by Hayden Hastings on 4/24/19.
-//  Copyright © 2019 Hayden Hastings. All rights reserved.
-//
-
-import Foundation
---
+++ ArtGallery/View Controllers/PaintingModel.swift
@@ -0,0 +1,46 @@
+//
+//  PainingModel.swift
+//  ArtGallery
+//
+//  Created by Hayden Hastings on 4/24/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Painting {
+    let image: UIImage
+    var isLiked: Bool
+    
+    init(image: UIImage, isLiked: Bool = false) {
+        self.image = image
+        self.isLiked = isLiked
+    }
+}
+
+protocol PaintingTableViewCellDelegate: class {
+    func tappedLikeButton(on cell: PaintingTableViewCell)
+}
+
+class PaintingModel: NSObject {
+    
+    
+}
+
+extension PaintingModel: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+extension PaintingModel: UITableViewDelegate {
+    
+}
