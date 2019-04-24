056287573912f461afe217ae130796968b11122b
Christopher Aronson <christopheraronson@icloud.com>
2019-04-24 14:28:26 -0700

    Started building interface and making IB connections.

---
+++ ArtGallery/ArtGallery/PaintingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PaintingViewController.swift
+//  ArtGallery
+//
+//  Created by Christopher Aronson on 4/24/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class PaintingViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
