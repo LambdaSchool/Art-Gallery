6802de2107515b630eb0e7e5784aa47433f697e7
Jeffrey Carpenter <spaz926@me.com>
2019-04-24 16:16:30 -0500

    Added table view and custom classes

---
+++ ArtGallery/ArtGallery/PaintingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaintingTableViewCell.swift
+//  ArtGallery
+//
+//  Created by Jeffrey Carpenter on 4/24/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class PaintingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
