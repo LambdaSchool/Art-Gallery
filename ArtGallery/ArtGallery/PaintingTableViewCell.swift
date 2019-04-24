a534aaf7364d1a716d4e34efe46f1fd1ec6e3673
Michael Redig <mredig@gmail.com>
2019-04-24 18:17:52 -0500

    completed the "set up your interface" section

--- ArtGallery/ArtGallery/PaintingTableViewCell.swift
+++ ArtGallery/ArtGallery/PaintingTableViewCell.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class PaintingTableViewCell: UITableViewCell {
-
+	@IBOutlet var paintingImageView: UIImageView!
+	
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,4 +22,6 @@ class PaintingTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+	@IBAction func likeButtonPressed(_ sender: Any) {
+	}
 }
