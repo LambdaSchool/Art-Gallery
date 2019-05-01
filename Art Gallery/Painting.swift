4d68d36b00023d689507777dd3b0c5f7c638ec06
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-04-30 23:59:32 -0400

    I started the project over after getting the better instructions.

---
+++ Art Gallery/Painting.swift
@@ -0,0 +1,15 @@
+//
+//  Painting.swift
+//  Art Gallery
+//
+//  Created by Nathan Hedgeman on 4/30/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Painting {
+    let image: UIImage
+    var isLiked = "Liked"
+}
