820fe0d9e221f84bc1e63b577c3c7aea543ddcce
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-05-01 15:25:05 -0400

    added images and PaintController class

---
+++ Art Gallery/PaintingController.swift
@@ -0,0 +1,27 @@
+//
+//  PaintingController.swift
+//  Art Gallery
+//
+//  Created by Nathan Hedgeman on 5/1/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PaintingController {
+    var paintings: [Painting] = []
+    
+    init() {
+        loadPaintingFromAssets()
+    }
+    
+    func loadPaintingFromAssets() {
+        for i in 1...12 {
+            let image: String = "Image \(i)"
+            guard let UIImage = UIImage(named: image) else {return}
+            let painting = Painting(image: UIImage)
+            paintings.append(painting)
+        }
+    }
+}
