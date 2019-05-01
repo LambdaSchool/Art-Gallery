42b5300355f613ea9c9e6cbf662ae27fa5a8ef95
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-05-01 13:50:11 -0400

    added Painting Class

--- Art Gallery/Painting.swift
+++ Art Gallery/Painting.swift
@@ -11,5 +11,10 @@ import UIKit
 
 class Painting {
     let image: UIImage
-    var isLiked = "Liked"
+    var isLiked : Bool
+    
+    init(image: UIImage, isLiked: Bool = false) {
+        self.image = image
+        self.isLiked = isLiked
+    }
 }
