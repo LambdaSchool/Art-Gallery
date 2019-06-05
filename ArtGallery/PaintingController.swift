2ef6ec720e36845476558a87c723b46cb842e81a
Kat Milton <mltnkat@gmail.com>
2019-06-05 12:57:49 -0700

    Finished like button toggle. Working on for-in loop for paintings array.

--- ArtGallery/PaintingController.swift
+++ ArtGallery/PaintingController.swift
@@ -15,15 +15,20 @@ class PaintingController {
     }
     var paintings: [Painting] = []
     
+    
     //Function to loop through paintings and name them as strings.
     
     func loadPaintingFromAssets() {
-        for image in 1..<paintings.count {
-            let painting = "Image\(image)"
-            
-        }
+//        for image in 1..<paintings.count {
+//            let images = "Image\(image)"
+//
+//        }
+        
     }
     
+    func toggleIsLiked(for painting: Painting) {
+        painting.isLiked.toggle()
+    }
     
     
 }
